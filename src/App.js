4b5d225adaf6bf28e206d5b5e4906bd85bf4ed4a
David <dhahn123@gmail.com>
2019-04-27 16:02:10 -0700

    Refactored v.4 Conditional Render

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
-// import { Body, Body2 } from './components/Body';
-// import Header from './components/Header';
-// import Counter from './components/Counter';
 import ImageSlider from "./components/ImageSlider";
 import Counter from "./components/Counter";
 
 class App extends Component {
   state = {
-    visible: true
+    visible: true,
+    whichComponentToShow: "ImageSlider"
   };
-  // add(a, b) {
-  //   return a + b;
-  // }
-
+ 
   render() {
-    let slider = this.state.visible ? (
-    <ImageSlider /> 
-    ) : (
-      <div>
-        <Counter />
-      </div>
-    );
-
-    // const add = (a, b) => a + b;
-    if (!this.state.visible) {
-      slider = <div>display nothing</div>;
+    if (this.state.whichComponentToShow === "ImageSlider") {
+      return (
+        <div className="App">
+          <ImageSlider />
+          <button onClick={() => {
+            this.setState({ whichComponentToShow:'Counter' })
+          }}
+          >
+          show counter
+          </button>
+        </div>
+      );
+    } else if (this.state.whichComponentToShow === "Counter") {
+      return (
+        <div className="App">
+          <Counter />
+        </div>
+      );
     }
 
-    const buttonText = this.state.visible ? 'hide' : 'show';
-
-    return (
-      <div className="App">
-        {slider}
-        <button
-          onClick={() => {
-            this.setState({
-              visible: !this.state.visible
-            });
-          }}
-        >
-          {buttonText}
-        </button>
-        {/* <Header
-          title={"Hello from Application"}
-          num={5}
-          myArr={[777, 2, 3]}
-          myFunc={this.add}
-          myObj={{
-            a: 5,
-            b: 6
-          }}
-        />
-        <Body2 />
-        <Body myFunc={this.add} text="i am cool" text2="i am cool2" />
-        <Counter initialCount={0} />
-        <Counter initialCount={100} /> */}
-      </div>
-    );
+    return null;
   }
 }
 
