992e71d19e63f40a4c4cd8468f9827f9497d6813
David <dhahn123@gmail.com>
2019-04-27 15:06:48 -0700

    State in React.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { Body, Body2 } from './components/Body';
 import Header from './components/Header';
+import Counter from './components/Counter';
 
 class App extends Component {
   add(a, b) {
@@ -24,6 +25,8 @@ class App extends Component {
         />
         <Body2 />
         <Body myFunc={this.add} text="i am cool" text2="i am cool2" />
+        <Counter initialCount={0} />
+        <Counter initialCount={100} />
       </div>
     );
   }
