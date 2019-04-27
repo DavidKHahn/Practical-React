ac0c6f75f20d10c903c7b363fff0c63d0d2c12e7
David <dhahn123@gmail.com>
2019-04-27 15:43:03 -0700

    Conditionally Render Components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import "./App.css";
 // import { Body, Body2 } from './components/Body';
 // import Header from './components/Header';
 // import Counter from './components/Counter';
-import ImageSlider from './components/ImageSlider';
+import ImageSlider from "./components/ImageSlider";
 
 class App extends Component {
-  add(a, b) {
-    return a + b;
-  }
+  state = {
+    visible: true
+  };
+  // add(a, b) {
+  //   return a + b;
+  // }
 
   render() {
     // const add = (a, b) => a + b;
+
+    const buttonText = this.state.visible ? 'hide' : 'show';
+
     return (
       <div className="App">
+        {this.state.visible ? <ImageSlider /> : <div>I am hidden</div>}
+        <button
+          onClick={() => {
+            this.setState({
+              visible: false
+            });
+          }}
+        >
+          {buttonText}
+        </button>
         {/* <Header
           title={"Hello from Application"}
           num={5}
@@ -28,7 +44,6 @@ class App extends Component {
         <Body myFunc={this.add} text="i am cool" text2="i am cool2" />
         <Counter initialCount={0} />
         <Counter initialCount={100} /> */}
-        <ImageSlider />
       </div>
     );
   }
