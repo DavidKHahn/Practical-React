b4643520edcb50dee306630ef36c65bc9e806b46
David <dhahn123@gmail.com>
2019-04-27 14:24:58 -0700

    Basic setup Create React.js Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+const Body = () => (
+  <p className="App-intro">
+  To get started, edit
+</p>
+);
+
+// const Body = () => {
+//   return <p className="App-intro">To get started, edit</p>;
+// };
+
+// function Body() {
+//   return (
+//     <p className="App-intro">
+//       To get started, edit
+//     </p>
+//   );
+// }
+
+class Header extends Component {
+  render() {
+    return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +37,19 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
