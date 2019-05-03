0c73b353cd31ea53f1d1143e9041bcefff0bc28d
David <dhahn123@gmail.com>
2019-05-02 20:49:54 -0700

    Form Validation Component

---
+++ src/components/ValidationForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default class ValidationForm extends React.Component {
+    state = {
+        name: "",
+        email: "",
+        password: ""
+    };
+
+    handleChange = event => {
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState({
+            [event.target.name]: isCheckbox
+            ? event.target.checked
+            : event.target.value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state);
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <input
+                        name="name"
+                        placeholder="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <input
+                        name="email"
+                        placeholder="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
